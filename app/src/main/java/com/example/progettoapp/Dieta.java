a40ce1fdc31187eecde1d503e67f40d147783b71
icerealguy <60849839+icerealguy@users.noreply.github.com>
2021-03-06 14:27:16 +0100

    Update and rename Dieta.kt to Dieta.java

---
+++ app/src/main/java/com/example/progettoapp/Dieta.java
@@ -0,0 +1,232 @@
+package com.example.progettoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class Dieta extends AppCompatActivity {
+
+    String strCosa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dieta_activity);
+
+        TextView giornoView;
+        TextView categoriaView;
+
+        Button inserisciBtn;
+        Button modificaBtn;
+        Button cercaBtn;
+
+        // Associa le view alle TextView rispettive
+        categoriaView = findViewById(R.id.textView3);
+        giornoView = findViewById(R.id.textView2);
+
+
+        // Associa i bottoni
+        inserisciBtn = findViewById(R.id.inserisciBtn);
+        modificaBtn = findViewById(R.id.modificaBtn);
+        cercaBtn = findViewById(R.id.cercaBtn);
+
+        Intent intent = getIntent();
+        String giorno = intent.getStringExtra("giorno");
+        String dieta = intent.getStringExtra("dieta");
+
+        ///////////////////////////////////////////////////////////////////////
+
+        File pathFile = getApplicationContext().getFilesDir();
+        String path= pathFile+"/dieta.txt";
+        File file = new File(path);
+        if (file.exists())
+            System.out.println("Il file " + path + " esiste");
+        else {
+            try {
+                if (file.createNewFile())
+                    System.out.println("Il file " + path + " è stato creato");
+                else
+                    System.out.println("Il file " + path + " non può essere creato");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+         strCosa = stampaDieta(file, giorno + dieta);
+
+        giornoView.setText(giorno);
+        categoriaView.setText(dieta);
+
+
+        //Bottoni
+        inserisciBtn.setOnClickListener(v -> inserisci(path,giorno+dieta));
+        modificaBtn.setOnClickListener(v -> modifica(path,giorno+dieta));
+        cercaBtn.setOnClickListener(v -> cerca(strCosa));
+
+    }
+
+    /**
+     *  Mostra la dieta richiesta e la stampa nella view
+     * @param file file contenente la dieta
+     * @param dieta combinazione pasto+giorno
+     */
+    private String stampaDieta(File file, String dieta) {
+
+        TextView risView;
+        risView = findViewById(R.id.textView4);
+
+        String str = "";
+        StringBuilder stringBuilder1 = new StringBuilder();
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            while (true) {
+                try {
+                    if ((str = br.readLine()) == null) break;
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if(str.equals(dieta))
+                {
+                    try {
+                        str=br.readLine();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    stringBuilder1.append(str).append("\n");
+                    //Commentato così stampa tutta l'alimentaione associata ad un giorno e tipo di dieta
+                    //break;
+                }
+            }
+
+            System.out.print("Il contenuto del file è il seguente: "+ stringBuilder1);
+            br.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        risView.setText(stringBuilder1);
+
+        System.out.println("return print " + stringBuilder1);
+        return stringBuilder1.toString();
+    }
+
+    /**
+     * Modifica il file inserendo la dieta associata ad un determinato giorno e pasto della giornata
+     * @param path Path del file della dieta
+     * @param dieta Pasto della giornata
+     */
+    private void modifica(String path, String dieta) {
+
+        EditText editText;
+        editText = findViewById(R.id.enterText);
+
+        String str = "";
+        StringBuilder stringBuilder1 = new StringBuilder();
+
+        try {
+            File file = new File(path);
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+
+            while (true) {
+                try {
+                    if ((str = br.readLine()) == null) break;
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if(str.equals(dieta))
+                {
+                    try {
+                        str=br.readLine();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    stringBuilder1.append(dieta).append("\n").append(editText.getText().toString()).append("\n");
+                }else{
+                    stringBuilder1.append(str).append("\n");
+                }
+
+
+            }
+
+            System.out.print("Il contenuto del file è il seguente: "+ stringBuilder1);
+            br.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            File file = new File(path);
+            FileWriter fw = new FileWriter(file, false);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.append(stringBuilder1);
+            bw.flush();
+            bw.close();
+            strCosa=stampaDieta(file,dieta);
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+    }
+
+    /**
+     * Inseriscenel file la dieta associata ad un determinato giorno e pasto della giornata
+     * @param path Path del file della dieta
+     * @param dieta Pasto della giornata
+     */
+    private void inserisci(String path, String dieta) {
+
+        EditText editText;
+        editText = findViewById(R.id.enterText);
+
+        try {
+            File file = new File(path);
+            FileWriter fw = new FileWriter(file, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.append(dieta+"\n"+editText.getText().toString()+"\n");
+            bw.flush();
+            bw.close();
+            strCosa=stampaDieta(file,dieta);
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Fa partire l'activity per cercare la ricetta su youtube
+     * @param pasto cosa cercare
+     */
+    private void cerca(String pasto) {
+
+        Intent intent = new Intent(this, VideoActivity.class);
+        intent.putExtra("cosa", pasto);
+
+        startActivity(intent);
+
+
+    }
+}
